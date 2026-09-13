75436e3df618e2f50d39cd40a53b40cdaef0381c
Daniel <Daniel.castrolopez@ucr.ac.cr>
2026-09-13 11:47:12 -0600

    Cambio de origen UCR -> GITHUB

---
+++ deportista/main.java
@@ -0,0 +1,40 @@
+/* Debe crear una clase para representar un deportista de una disciplina de su elección .
+
+Defina cuatro atributos de tipos de dato diferente que sean importantes para describir a un deportista de esa disciplina.
+
+En un Main debe crear tres instancias con atributos definidos por el usuario leyendo los datos desde entrada estándar .
+
+Luego de crear las tres instancias debe imprimir su información en una tabla descriptiva con los atributos de sus tres deportistas
+
+
+Suban el código fuente (archivos .java) y un README.md creado por ustedes explicando brevemente su implementación. Debe comprimir estos archivos en un .zip y subir este archivo comprimido.
+
+El trabajo debe ser individual
+
+import java.util.Scanner;
+
+scanner lector =
+ */
+
+public static void Main (String [] args) {
+
+
+}
+
+public class Deportista {
+
+private String nombre;
+private int edad;
+private float altura;
+private char genero;
+
+}
+
+public Deportista (String nombre, int edad, float altura, char genero) {
+
+	this.nombre = "Lionel Messi"
+	this.edad = 39
+	this.altua = 1.70
+	this.genero = "M"
+
+}
